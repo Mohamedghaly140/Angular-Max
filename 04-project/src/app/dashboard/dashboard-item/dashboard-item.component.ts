1f169bb56ee9400af502bb25b036d5f6f16fbd70
mohamedghaly140 <mohamedghaly140@gmail.com>
2024-09-09 15:16:09 +0300

    split and create reuseable components

---
+++ 04-project/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, input } from '@angular/core';
+
+type Image = { src: string; alt: string };
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css',
+})
+export class DashboardItemComponent {
+  // @Input({ required: true }) image!: Image;
+  // @Input({ required: true }) title = '';
+  image = input.required<Image>();
+  title = input.required<string>();
+}
