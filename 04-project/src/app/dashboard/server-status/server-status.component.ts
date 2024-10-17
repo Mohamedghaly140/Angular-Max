f304a674a827187df52abd4b8c6762f9774555e2
mohamedghaly140 <mohamedghaly140@gmail.com>
2024-10-17 21:20:41 +0300

    added lifecycle methods

--- 04-project/src/app/dashboard/server-status/server-status.component.ts
+++ 04-project/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+type ServerStatus = 'online' | 'offline' | 'unknown';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit, OnDestroy {
+  currentStatus: ServerStatus = 'online';
+
+  private intervalId?: ReturnType<typeof setInterval>;
+
+  ngOnInit(): void {
+    this.intervalId = setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
